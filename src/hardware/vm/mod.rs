e7d0d7bfa7258aa0f03c26e9140b4ee7e6b9dd81
luciano-eth <113019900+luciano-eth@users.noreply.github.com>
2023-11-11 23:54:20 +0100

    extending read_memory function capability

--- src/hardware/vm/mod.rs
+++ src/hardware/vm/mod.rs
@@ -13,7 +13,21 @@ impl VM {
     }
 
     pub fn read_memory(&mut self, address: u16) -> u16 {
+        if address == MemoryMappedReg::kbsr as u16 {
+            self.handle_keyboard();
+        }
         self.memory[address as usize]
+    }
+
+    fn handle_keyboard(&mut self) {
+        let mut buffer = [0; 1];
+        std::io_stdin().read_exact(&mut buffer).unwrap();
+        if buffer[0] != 0 {
+            self.write_memory(MemoryMappedReg::kbsr as usize, 1 << 15);
+            self.write_memory(MemoryMappedReg::kbdr as usize, buffer[0] as u16);
+        } else {
+            self.write_memory(MemoryMappedReg::kbsr as uszie, 0)
+        }
     }
 
     pub fn write_memory(&mut self, address: usize, value: u16) {
